0021b3718c9db5c8e43fc18fe6138cdc0d092d9c
greg@gregPredator <greg19850@gmail.com>
2022-07-10 18:39:37 +0100

    created function drawing random name and placing it in sentence in div

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const names = ['Andy', 'Marry', 'Rob', 'Anna', 'Silvia', 'Agness', 'Jack', 'Matt
 const prefixes = ['I think', 'In my opinion', 'I am absolutely sure', 'I have no doubts', 'I would suggest'];
 
 const nameSelect = () =>{
+let nameIndex = Math.floor(Math.random() * names.length);
+let prefixIndex = Math.floor(Math.random() * prefixes.length);
 
+document.querySelector('div').innerHTML = `${prefixes[prefixIndex]}, that you should name your baby <strong>${names[nameIndex]}</strong>`
 }
 
 btn.addEventListener('click', nameSelect)
