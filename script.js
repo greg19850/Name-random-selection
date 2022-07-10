aaa3788926c4466155412b18a8bc98e7b730d977
greg@gregPredator <greg19850@gmail.com>
2022-07-10 18:26:28 +0100

    created two arrays, with names & prefixes. Declaired function to choose random name

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('button');
+
+const names = ['Andy', 'Marry', 'Rob', 'Anna', 'Silvia', 'Agness', 'Jack', 'Matt'];
+const prefixes = ['I think', 'In my opinion', 'I am absolutely sure', 'I have no doubts', 'I would suggest'];
+
+const nameSelect = () =>{
+
+}
+
+btn.addEventListener('click', nameSelect)
